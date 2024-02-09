ffea267df6a4571580936d5ad4f141bc90cda0d0
Monika Derewecka <monika.derewecka@schibsted.com>
2024-02-09 13:36:10 +0100

    invoice implementation

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,52 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.*;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private Map<Product, Integer> products;
+
+    public Invoice() {
+        this.products = new HashMap<>();
+    }
 
     public void addProduct(Product product) {
-        // TODO: implement
+        addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (quantity <= 0 || product == null) {
+            throw new IllegalArgumentException("Ilość musi być większa niż 0");
+        } else {
+            products.put(product, quantity);
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> prod : products.entrySet()) {
+            Product product = prod.getKey();
+            Integer quantity = prod.getValue();
+            BigDecimal price = product.getPrice().multiply(new BigDecimal(quantity));
+            subtotal = subtotal.add(price);
+        }
+        return subtotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal totalTax = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> prod : products.entrySet()) {
+            Product product = prod.getKey();
+            Integer quantity = prod.getValue();
+            BigDecimal tax = product.getPriceWithTax().subtract(product.getPrice()).multiply(new BigDecimal(quantity));
+            totalTax = totalTax.add(tax);
+        }
+        return totalTax;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        return getSubtotal().add(getTax());
     }
 }
